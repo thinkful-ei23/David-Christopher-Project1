8645b384594be808e1f00ef5f31a2c7618c71dda
David Graves <davidgraves831@gmail.com>
2018-07-16 14:42:38 -0500

    fixed conditions

--- set1.js
+++ set1.js
@@ -1,22 +1,22 @@
 function whoAmI(name, age) {
 var yob = yearOfBirth(age);
-if (!name) {
+if (!name || typeof name !== "string") {
   throw new Error("Argument not valid");
 }
-console.log(`Hi, my ${name} is Chris and I'm 20 years old`);
+console.log(`Hi, my name is ${name} and I'm 20 years old`);
 console.log(`I was born in ${yob}`);
 
 }
 
 function yearOfBirth(age) {
-if (!age || age < 0){
+if (!age || age < 0 || typeof age !== "number"){
   throw new Error("Agrgument not valid");
 }
 return 2018 - age;
 }
 
 try{
-  whoAmI("Chris", -5);
+  whoAmI("Chris" , "Chris");
 }
 catch(e){
   console.error(e.message);
