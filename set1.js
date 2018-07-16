36da323711847e7ebf4f99ada44e46b488da33d5
David Graves <davidgraves831@gmail.com>
2018-07-16 14:19:05 -0500

    added try catch, cleaned it up

--- set1.js
+++ set1.js
@@ -5,10 +5,25 @@ console.log(`Hi, my name is Chris and I'm 20 years old`);
 console.log(`I was born in ${yearOfBirth}`);
 }
 
-whoAmI();
-
 function yearOfBirth(age) {
-let yearOfBirth = 2018 - age;
+throw new Error("Age can not be negative");
+console.log(`I was born in ${yearOfBirth}`);
+return 2018 - age; 
+}
+
+whoAmI("Chris", -5function whoAmI(name, age) {
+var yob = yearOfBirth(age);
+console.log(`Hi, my name is Chris and I'm 20 years old`);
 console.log(`I was born in ${yearOfBirth}`);
 }
+
+function yearOfBirth(age) {
+throw new Error("Age can not be negative");
+console.log(`I was born in ${yearOfBirth}`);
+return 2018 - age;
+}
+
+whoAmI("Chris", -5);
+);
+
 
