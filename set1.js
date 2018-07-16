d6effa473699264a6f89c95d2195140362751938
David Graves <davidgraves831@gmail.com>
2018-07-16 13:47:36 -0500

    added yearOfBirth

--- set1.js
+++ set1.js
@@ -1,6 +1,8 @@
 
 function whoAmI(name, age) {
+let yearOfBirth = 2018 - age;
 console.log(`Hi, my name is Chris and I'm 20 years old`);
+console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI();
