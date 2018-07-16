0918a77bd3894166d20d442f5978ead66eaa3dee
Chris Gantz <chrisgantz836@gmail.com>
2018-07-16 12:31:45 -0700

    conditionals added

--- set1.js
+++ set1.js
@@ -1,12 +1,16 @@
 function whoAmI(name, age) {
 var yob = yearOfBirth(age);
+if (!name) {
+  throw new Error("Argument not valid");
+}
 console.log(`Hi, my ${name} is Chris and I'm 20 years old`);
 console.log(`I was born in ${yob}`);
+
 }
 
 function yearOfBirth(age) {
-if (age < 0){
-  throw new Error("Age can not be negative");
+if (!age || age < 0){
+  throw new Error("Agrgument not valid");
 }
 return 2018 - age;
 }
