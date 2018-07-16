445bd619e7bffba00d6fef1b71694d8d572f291b
Chris Gantz <chrisgantz836@gmail.com>
2018-07-16 11:59:41 -0700

    made YOB a function

--- set1.js
+++ set1.js
@@ -1,8 +1,14 @@
 
 function whoAmI(name, age) {
-let yearOfBirth = 2018 - age;
+var yob = yearOfBirth(age);
 console.log(`Hi, my name is Chris and I'm 20 years old`);
 console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI();
+
+function yearOfBirth(age) {
+let yearOfBirth = 2018 - age;
+console.log(`I was born in ${yearOfBirth}`);
+}
+
