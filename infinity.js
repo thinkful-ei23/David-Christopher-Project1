3cd4a17c4ae35e35bd1219a361b48f1e913f60af
Chris Gantz <chrisgantz836@gmail.com>
2018-07-16 13:23:10 -0700

    To infinity.js added

---
+++ infinity.js
@@ -0,0 +1,15 @@
+function beyond(num) {
+if (num > 0) {
+console.log("To infinity");
+}
+if (num < 0) {
+console.log("To negative infinity");
+}
+if (num > 0 || num < 0) {
+console.log("And beyond");
+}
+else {
+console.log("Staying home");
+}
+}
+beyond(-5);
